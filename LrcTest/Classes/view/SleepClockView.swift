fa14cac5fe311f23e7342d561591071b62a72bf1
pengyucheng <pengyucheng@huoshug.local>
2017-03-20 18:03:38 +0800

    添加定时器伪代码

---
+++ LrcTest/Classes/view/SleepClockView.swift
@@ -0,0 +1,41 @@
+//
+//  SleepClockView.swift
+//  LrcTest
+//
+//  Created by pengyucheng on 20/03/2017.
+//  Copyright © 2017 PBBReader. All rights reserved.
+//
+
+import UIKit
+
+class SleepClockView: UIAlertController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    //根据给定数组，定制View
+    //
+    
+    //定时事件：开启定时器，到时间关闭播放器
+    
+    //代理协议：宿主程序中代理关闭播放器事件
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
