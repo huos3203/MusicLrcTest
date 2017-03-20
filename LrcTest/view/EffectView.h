2868a65b6d0d7e40afd635c7eaf9a35dfb091b37
huoshuguang <huoshuguang@2fd7f1eb-dab2-644b-9d89-fcf9eb30962b>
2017-03-20 08:00:59 +0000

    添加毛玻璃效果
    git-svn-id: https://192.168.85.6/svn/DRM_iOS/trunk/MusicLrc@2859 2fd7f1eb-dab2-644b-9d89-fcf9eb30962b

---
+++ LrcTest/view/EffectView.h
@@ -0,0 +1,13 @@
+//
+//  EffectView.h
+//  LrcTest
+//
+//  Created by pengyucheng on 14/03/2017.
+//  Copyright © 2017 PBBReader. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EffectView : UIImageView
+
+@end
