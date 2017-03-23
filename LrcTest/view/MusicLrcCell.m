61f8936fe0f7a6d995b47f4c65c5ff19f909b4e0
huoshuguang <huoshuguang@2fd7f1eb-dab2-644b-9d89-fcf9eb30962b>
2017-03-23 09:23:53 +0000

    当前歌词支持填充色渐变，放大字号，定位到屏幕剧中位置
    git-svn-id: https://192.168.85.6/svn/DRM_iOS/trunk/MusicLrc@2930 2fd7f1eb-dab2-644b-9d89-fcf9eb30962b

---
+++ LrcTest/view/MusicLrcCell.m
@@ -0,0 +1,64 @@
+//
+//  MusicLrcCell.m
+//  LrcTest
+//
+//  Created by pengyucheng on 14/03/2017.
+//  Copyright © 2017 PBBReader. All rights reserved.
+//
+
+#import "MusicLrcCell.h"
+
+@implementation MusicLrcCell
+
++(MusicLrcCell *)lrcCellWithTableView:(UITableView *)tableView
+{
+    static NSString *ID = @"cell";
+    MusicLrcCell *cell = [tableView dequeueReusableCellWithIdentifier:ID];
+    if (!cell) {
+        cell = [[MusicLrcCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:ID];
+    }
+    return cell;
+    
+}
+
+
+- (instancetype)initWithStyle:(UITableViewCellStyle)style
+              reuseIdentifier:(NSString *)reuseIdentifier
+{
+    if (self = [super initWithStyle:style reuseIdentifier:reuseIdentifier])
+    {
+        //设置
+        self.backgroundColor = [UIColor clearColor];
+        self.selectionStyle = UITableViewCellSelectionStyleNone;
+        //添加子控件
+        MusicLrcLabel *lrcLabel = [[MusicLrcLabel alloc]init];
+        [self.contentView addSubview:lrcLabel];
+        _lrcLabel = lrcLabel;
+        lrcLabel.textColor = _lrcColor;
+        lrcLabel.font = [UIFont systemFontOfSize:14];
+        lrcLabel.textAlignment = NSTextAlignmentCenter;
+        //约束
+        lrcLabel.translatesAutoresizingMaskIntoConstraints = NO;
+//        [lrcLabel mas_makeConstraints:^(MASConstraintMaker *make) {
+//            make.center.equalTo(self.contentView);
+//        }];
+        
+        NSArray *h = [NSLayoutConstraint constraintsWithVisualFormat:@"H:|-0-[lrcLabel]-0-|"
+                                                             options:0 metrics:nil
+                                                               views:@{@"lrcLabel":lrcLabel}];
+        NSArray *v = [NSLayoutConstraint constraintsWithVisualFormat:@"V:|-0-[lrcLabel]-0-|"
+                                                             options:0 metrics:nil
+                                                               views:@{@"lrcLabel":lrcLabel}];
+        [self.contentView addConstraints:h];
+        [self.contentView addConstraints:v];
+    }
+    return self;
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
