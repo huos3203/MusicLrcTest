325b27a025ea3bbf5c1a87e751fe2aedd4e777c6
huoshuguang <huoshuguang@2fd7f1eb-dab2-644b-9d89-fcf9eb30962b>
2017-03-23 09:24:02 +0000

    添加倒计时实现类
    git-svn-id: https://192.168.85.6/svn/DRM_iOS/trunk/MusicLrc@2948 2fd7f1eb-dab2-644b-9d89-fcf9eb30962b

--- LrcTest/Classes/view/SleepClockView.swift
+++ LrcTest/Classes/view/SleepClockView.swift
@@ -5,14 +5,34 @@
 //  Created by pengyucheng on 20/03/2017.
 //  Copyright © 2017 PBBReader. All rights reserved.
 //
-
+//chttp://www.cnblogs.com/QianChia/p/5777473.html
 import UIKit
+//委托延迟设置，取消延迟
+@objc
+protocol DelayDelegate
+{
+    //设置delay
+    func setDelayToPerformCloseOperation()
+    //开始
+    func startCountingDownUntilExecution()
+    //取消
+    func cancelPerformCloseOperation()
+}
+
+
 
 class SleepClockView: UIAlertController {
 
+    class var shareInstance:SleepClockView
+    {
+        struct Singleton{
+            static let instance = SleepClockView()
+        }
+        return Singleton.instance
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
     }
 
@@ -22,20 +42,55 @@ class SleepClockView: UIAlertController {
     }
     
     //根据给定数组，定制View
-    //
+    private var delayDelegate:DelayDelegate?
+    private var delayTime:TimeInterval = 0.0
+    @objc(setupBy:toTarget:)
+    func setup(clockArr:[Int] = [1,2,3,4] ,toTarget Delgate:DelayDelegate)
+    {
+        self.delayDelegate = Delgate
+        //
+        let cancelAction = UIAlertAction.init(title: "暂时不取消设置", style: .default) { (acion) in
+                self.delayTime = 0.0
+                self.cancelClock()
+        }
+        self.addAction(cancelAction)
+        let clock = clockArr.sorted(by: >)
+        for(index,clock) in clock.enumerated()
+        {
+            let action = UIAlertAction.init(title: "\(clock) 分钟)",
+                style: .default, handler: { (action) in
+                    //事件
+                    self.delayTime = TimeInterval(clockArr[index])
+            })
+            self.addAction(action)
+        }
+    }
+    
+    //创建定时器
+    func createClock()
+    {
+        
+        self.delayDelegate?.setDelayToPerformCloseOperation()
+    }
     
     //定时事件：开启定时器，到时间关闭播放器
-    
-    //代理协议：宿主程序中代理关闭播放器事件
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func startClock()
+    {
+        self.perform(#selector(SleepClockView.createClock), with:nil, afterDelay: self.delayTime)
     }
-    */
+    
+    //取消定延迟调用
+    func cancelClock()
+    {
+        NSObject.cancelPreviousPerformRequests(withTarget: self,
+                                               selector: #selector(SleepClockView.cancelByClock),
+                                               object: nil)
+    }
+   
+    //代理协议：宿主程序中代理关闭播放器事件
+    func cancelByClock()
+    {
+        self.delayDelegate?.cancelPerformCloseOperation()
+    }
 
 }
