c4388dec81dd95c743cadb090363dcbb7b9d47fb
Xcode Server <xcodeserver@server.local>
2017-03-24 14:12:20 +0800

    JITHH：:

---
+++ LrcTest/ClockViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ClockViewController.swift
+//  LrcTest
+//
+//  Created by Xcode Server on 2017/3/24.
+//  Copyright © 2017年 PBBReader. All rights reserved.
+//
+
+import UIKit
+
+class ClockViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        //persentation按钮
+        
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
