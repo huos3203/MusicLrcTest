fd38b559cc42d9d15fe9feb0de7362ec6c1fad0b
Xcode Server <xcodeserver@server.local>
2017-03-28 20:42:25 +0800

    app更新提示

--- LrcTest/Classes/HTTP/HttpClientManager.swift
+++ LrcTest/Classes/HTTP/HttpClientManager.swift
@@ -35,17 +35,20 @@ public class HttpClientManager:NSObject
         let dataTask: URLSessionDataTask = URLSession.shared.dataTask(with: request) {
             (data, resp, err) in
            // print("响应的服务器地址：\(resp?.url?.absoluteString)")
-            var dict:NSDictionary? = nil
-            do {
-                dict  = try JSONSerialization.jsonObject(with: data!, options: JSONSerialization.ReadingOptions.init(rawValue: 0)) as? NSDictionary
-        
-                app.code = dict?.object(forKey: "code") as! String
-                let resu = dict?.object(forKey: "result") as! NSNumber
-                app.result = resu.boolValue
-                app.msg = dict?.object(forKey: "msg") as! String
-                completionHander(app)
-            } catch {
-                
+            let response = resp as! HTTPURLResponse
+            if(response.statusCode == 200)
+            {
+                var dict:NSDictionary? = nil
+                do {
+                    dict  = try JSONSerialization.jsonObject(with: data!, options: JSONSerialization.ReadingOptions.init(rawValue: 0)) as? NSDictionary
+                    app.code = dict?.object(forKey: "code") as! String
+                    let resu = dict?.object(forKey: "result") as! NSNumber
+                    app.result = resu.boolValue
+                    app.msg = dict?.object(forKey: "msg") as! String
+                    completionHander(app)
+                } catch {
+                    print("验证app版本，解析时失败")
+                }
             }
         }
         dataTask.resume()
@@ -68,10 +71,12 @@ public class HttpClientManager:NSObject
         request.httpMethod = "POST"
         let requestLrcURL = URLSession.shared.dataTask(with: request) { (data, response, err) in
             //
-            if data != nil
+            let responses = response as! HTTPURLResponse
+            print("responses.statusCode：-----\(responses.statusCode)")
+            if (responses.statusCode == 200)
             {
                 let httpURL = String.init(data: data!, encoding: String.Encoding.utf8)
-             
+                
                 if (httpURL?.lowercased().hasSuffix("lrc"))!
                 {
                     self.downLRCFile(urlStr: httpURL!,
@@ -83,9 +88,7 @@ public class HttpClientManager:NSObject
                     print("文件路径错误")
                     loadLrc("")
                 }
-                
             }
-            
         }
         requestLrcURL.resume()
     }
@@ -135,7 +138,6 @@ public class HttpClientManager:NSObject
     //
     public func loadLrcBy(lrcModel:MusicLrcModel,player:AVAudioPlayer,lrcDelegate:MusicLrcDelegate,completion:@escaping (Bool)->Void)
     {
-    
         MusicLrcView.shared().showindicatorView()
         downMusicLrcBy(lrcModel: lrcModel) { (lrcPath) in
             //
