8154fee74c3b0c50e66507ab5c5fc83545ecb962
huoshuguang <huoshuguang@2fd7f1eb-dab2-644b-9d89-fcf9eb30962b>
2017-04-17 08:26:28 +0000

    完善歌词下载格式失败详情
    git-svn-id: https://192.168.85.6/svn/DRM_iOS/trunk/MusicLrc@3208 2fd7f1eb-dab2-644b-9d89-fcf9eb30962b

--- LrcTest/Classes/HTTP/HttpClientManager.swift
+++ LrcTest/Classes/HTTP/HttpClientManager.swift
@@ -19,41 +19,6 @@ public class HttpClientManager:NSObject
         return Singleton.instance
     }
     
-    
-    
-    //请求url
-    public func verifyAppVersionBy(withURL url:String,
-                                     about app:VerifyAppVersionModel,
-                              completionHander:@escaping (VerifyAppVersionModel)->Void)
-    {
-        //urlsession使用
-        var request = URLRequest(url: URL(string: url)!)
-        print(app.convertToJSON())
-        let data = app.convertToJSON().data(using: .utf8)
-        request.httpBodyStream = InputStream.init(data: data!)
-        request.httpMethod = "POST"
-        let dataTask: URLSessionDataTask = URLSession.shared.dataTask(with: request) {
-            (data, resp, err) in
-           // print("响应的服务器地址：\(resp?.url?.absoluteString)")
-            let response = resp as! HTTPURLResponse
-            if(response.statusCode == 200)
-            {
-                var dict:NSDictionary? = nil
-                do {
-                    dict  = try JSONSerialization.jsonObject(with: data!, options: JSONSerialization.ReadingOptions.init(rawValue: 0)) as? NSDictionary
-                    app.code = dict?.object(forKey: "code") as! String
-                    let resu = dict?.object(forKey: "result") as! NSNumber
-                    app.result = resu.boolValue
-                    app.msg = dict?.object(forKey: "msg") as! String
-                    completionHander(app)
-                } catch {
-                    print("验证app版本，解析时失败")
-                }
-            }
-        }
-        dataTask.resume()
-    }
-    
     //下载歌词
     public func downMusicLrcBy(lrcModel model:MusicLrcModel , loadLrc:@escaping (String,String)->Void)
     {
@@ -78,6 +43,118 @@ public class HttpClientManager:NSObject
         self.requestLRCURLBy(model: model, loadLrc: loadLrc)
     }
     
+    /// 请求歌词路径
+    func requestLRCURLBy(model:MusicLrcModel, loadLrc:@escaping (String,String)->Void)
+    {
+        ///此处无需转码：addingPercentEncoding(withAllowedCharacters: .urlPathAllowed)
+        let urladd = "\(model.lrcURL!)?\(model.convertToJSON())"
+        let url = URL(string:urladd)!
+        do {
+            let httpURL = try NSString.init(contentsOf: url, encoding: String.Encoding.utf8.rawValue)
+            //print(htmlsource)
+            if (httpURL.lowercased.hasSuffix("lrc"))
+            {
+                //test
+                //let lrcFileURL = self.createLRCDir().path
+                let lrcFileURL = model.localPath!
+                self.downLRCByStringURL(urlStr:  httpURL as String, fileName: lrcFileURL, loadLrc: loadLrc)
+            }
+            else
+            {
+                //print("文件路径错误")
+                loadLrc("","lrc路径错误：\(httpURL)")
+            }
+            
+        } catch
+        {
+            //
+            loadLrc("","请求lrc文件路径失败，下载错误\(error.localizedDescription)")
+        }
+
+    }
+    /// 下载歌词
+    func downLRCByStringURL(urlStr:String,fileName:String ,loadLrc:@escaping (String,String)->Void)
+    {
+        //   
+        let urlStr1 = urlStr.addingPercentEncoding(withAllowedCharacters: .urlPathAllowed)
+        let url = URL(string:urlStr1!)!
+        let lrcFileURL = URL.init(fileURLWithPath: fileName)
+        var encoding:UInt
+        do
+        {
+            // 获得编码ID
+            encoding = CFStringConvertEncodingToNSStringEncoding(CFStringEncoding(CFStringEncodings.GB_18030_2000.rawValue))
+            try sourceByEncoding(sourceURL: url, lrcFileURL: lrcFileURL, encoding: encoding)
+            loadLrc(lrcFileURL.path,"")
+        }
+        catch
+        {
+            do {
+                encoding = String.Encoding.utf8.rawValue
+                try sourceByEncoding(sourceURL: url, lrcFileURL: lrcFileURL, encoding: encoding)
+                loadLrc(lrcFileURL.path,"下载歌词，gbk失败：\(error.localizedDescription)\n 开始使用utf8格式下载")
+            } catch
+            {
+                print(error.localizedDescription)
+                loadLrc("","lrc文件存盘中失败:\(error.localizedDescription)")
+            }
+            
+        }
+        
+    }
+    
+    /// 通过指定的编码格式下载歌词
+    func sourceByEncoding(sourceURL:URL,lrcFileURL:URL,encoding:UInt) throws
+    {
+        let htmlsource = try NSString.init(contentsOf: sourceURL, encoding: encoding)
+        try htmlsource.write(to: lrcFileURL, atomically: true, encoding: String.Encoding.utf8.rawValue)
+    }
+    
+    
+    
+    //MARK: 测试
+    func createLRCDir()->URL
+    {
+        let documentPath = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).first
+        let lrcDirURL = URL.init(fileURLWithPath: documentPath!)
+        do
+        {
+            try FileManager.default.createDirectory(at: lrcDirURL, withIntermediateDirectories: true, attributes: nil)
+        }catch{
+            print("=====")
+        }
+        
+        return lrcDirURL.appendingPathComponent("test.lrc")
+    }
+}
+
+
+
+extension HttpClientManager
+{
+    
+    ///下载完成后，同步歌词
+    public func loadLrcBy(lrcModel:MusicLrcModel,player:AVAudioPlayer,lrcDelegate:MusicLrcDelegate,completion:@escaping (Bool,String)->Void)
+    {
+        MusicLrcView.shared().showindicatorView()
+        downMusicLrcBy(lrcModel: lrcModel) { (lrcPath,logmessage) in
+            //
+            OperationQueue.main.addOperation {
+                let isCanLoad = MusicLrcView.shared().loadLrc(by: lrcPath, audioPlayer: player, lrcDedegate: lrcDelegate)
+                completion(isCanLoad,logmessage)
+            }
+        }
+    }
+    
+    ///移除歌词同步定时器
+    public func removeLrcTimer()
+    {
+        MusicLrcView.shared().removeLrcTimer()
+    }
+}
+
+extension HttpClientManager
+{
     /// 访问请求歌词路径的接口，出现501的频率很高
     func downLRCFile(urlStr:String,fileName:String ,loadLrc:@escaping (String)->Void)
     {
@@ -158,105 +235,42 @@ public class HttpClientManager:NSObject
         }
         requestLrcURL.resume()
     }
-    
-    
-    func requestLRCURLBy(model:MusicLrcModel, loadLrc:@escaping (String,String)->Void)
-    {
-        ///此处无需转码：addingPercentEncoding(withAllowedCharacters: .urlPathAllowed)
-        let urladd = "\(model.lrcURL!)?\(model.convertToJSON())"
-        let url = URL(string:urladd)!
-        do {
-            let httpURL = try NSString.init(contentsOf: url, encoding: String.Encoding.utf8.rawValue)
-            //print(htmlsource)
-            if (httpURL.lowercased.hasSuffix("lrc"))
-            {
-                //test
-                //let lrcFileURL = self.createLRCDir().path
-                let lrcFileURL = model.localPath!
-                self.downLRCByStringURL(urlStr:  httpURL as String, fileName: lrcFileURL, loadLrc: loadLrc)
-            }
-            else
-            {
-                //print("文件路径错误")
-                loadLrc("","lrc错误路径：\(httpURL)")
-            }
-            
-        } catch
-        {
-            //
-            loadLrc("","请求lrc文件路径失败")
-        }
 
-    }
-    
-    func downLRCByStringURL(urlStr:String,fileName:String ,loadLrc:@escaping (String,String)->Void)
-    {
-        //   
-        let urlStr1 = urlStr.addingPercentEncoding(withAllowedCharacters: .urlPathAllowed)
-        let url = URL(string:urlStr1!)!
-        let lrcFileURL = URL.init(fileURLWithPath: fileName)
-        var encoding:UInt
-        do
-        {
-            // 获得编码ID
-            encoding = CFStringConvertEncodingToNSStringEncoding(CFStringEncoding(CFStringEncodings.GB_18030_2000.rawValue))
-            try sourceByEncoding(sourceURL: url, lrcFileURL: lrcFileURL, encoding: encoding)
-            loadLrc(lrcFileURL.path,"")
-        }
-        catch
-        {
-            print(error.localizedDescription)
-            //
-            do {
-                encoding = String.Encoding.utf8.rawValue
-                try sourceByEncoding(sourceURL: url, lrcFileURL: lrcFileURL, encoding: encoding)
-                loadLrc(lrcFileURL.path,"")
-            } catch
-            {
-                print(error.localizedDescription)
-                loadLrc("","lrc文件存盘中失败:\(error.localizedDescription)")
-            }
-            
-        }
-        
-    }
+}
 
-    func sourceByEncoding(sourceURL:URL,lrcFileURL:URL,encoding:UInt) throws
+///版本号验证
+extension HttpClientManager
+{
+    //请求url
+    public func verifyAppVersionBy(withURL url:String,
+                                   about app:VerifyAppVersionModel,
+                                   completionHander:@escaping (VerifyAppVersionModel)->Void)
     {
-        let htmlsource = try NSString.init(contentsOf: sourceURL, encoding: encoding)
-        try htmlsource.write(to: lrcFileURL, atomically: true, encoding: String.Encoding.utf8.rawValue)
-    }
-    
-    
-    func createLRCDir()->URL
-    {
-        let documentPath = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).first
-        let lrcDirURL = URL.init(fileURLWithPath: documentPath!)
-        do
-        {
-            try FileManager.default.createDirectory(at: lrcDirURL, withIntermediateDirectories: true, attributes: nil)
-        }catch{
-            print("=====")
-        }
-        
-        return lrcDirURL.appendingPathComponent("test.lrc")
-    }
-    
-    //
-    public func loadLrcBy(lrcModel:MusicLrcModel,player:AVAudioPlayer,lrcDelegate:MusicLrcDelegate,completion:@escaping (Bool,String)->Void)
-    {
-        MusicLrcView.shared().showindicatorView()
-        downMusicLrcBy(lrcModel: lrcModel) { (lrcPath,logmessage) in
-            //
-            OperationQueue.main.addOperation {
-                let isCanLoad = MusicLrcView.shared().loadLrc(by: lrcPath, audioPlayer: player, lrcDedegate: lrcDelegate)
-                completion(isCanLoad,logmessage)
+        //urlsession使用
+        var request = URLRequest(url: URL(string: url)!)
+        print(app.convertToJSON())
+        let data = app.convertToJSON().data(using: .utf8)
+        request.httpBodyStream = InputStream.init(data: data!)
+        request.httpMethod = "POST"
+        let dataTask: URLSessionDataTask = URLSession.shared.dataTask(with: request) {
+            (data, resp, err) in
+            // print("响应的服务器地址：\(resp?.url?.absoluteString)")
+            let response = resp as! HTTPURLResponse
+            if(response.statusCode == 200)
+            {
+                var dict:NSDictionary? = nil
+                do {
+                    dict  = try JSONSerialization.jsonObject(with: data!, options: JSONSerialization.ReadingOptions.init(rawValue: 0)) as? NSDictionary
+                    app.code = dict?.object(forKey: "code") as! String
+                    let resu = dict?.object(forKey: "result") as! NSNumber
+                    app.result = resu.boolValue
+                    app.msg = dict?.object(forKey: "msg") as! String
+                    completionHander(app)
+                } catch {
+                    print("验证app版本，解析时失败")
+                }
             }
         }
-    }
-    
-    public func removeLrcTimer()
-    {
-        MusicLrcView.shared().removeLrcTimer()
+        dataTask.resume()
     }
 }
